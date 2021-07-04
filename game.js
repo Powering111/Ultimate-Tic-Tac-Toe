5e3d122ea1b9c41ddbbaf007e3fc97da829ba438
Powering111 <bestjun111@gmail.com>
2021-07-04 17:59:08 +0900

    asd

--- game.js
+++ game.js
@@ -1,8 +1,39 @@
+var gameObject1,gameObject2,gameObject3;
+var canvas,context;
+function startGame(){
+  canvas = document.getElementById("game");
+  context = canvas.getContext("2d");
 
-document.addEventListener("DOMContentLoaded",function(){
-  var canvas = document.getElementById("game");
-  var context = canvas.getContext("2d");
-  context.moveTo(0,0);
-  context.lineTo(200,100);
-  context.stroke();
-});
+  gameObject1=new component(0,0,100,100,"red");
+  gameObject2 = new component(300,30,20,20,"yellow");
+  gameObject3 = new component(50,10,30,5,"blue");
+
+
+  var interval=setInterval(updateCanvas,20);
+
+}
+
+function component(x,y,width,height,color){
+  this.x=x;
+  this.y=y;
+  this.width=width;
+  this.height=height;
+  this.color=color;
+  this.update=function(){
+    context.fillStyle=color;
+    context.fillRect(this.x,this.y,this.width,this.height);
+  }
+}
+
+function updateCanvas(){
+  context.clearRect(0,0,canvas.width,canvas.height);
+  context.fillStyle="#99FF99";
+  context.fillRect(0,0,500,500);
+  context.fillStyle="#0000FF";
+
+  gameObject1.update();
+  gameObject2.update();
+  gameObject3.update();
+
+
+}
