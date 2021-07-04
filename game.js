fdddb06d664aa67bc4ad65adeb4f928fdfcf5af6
Powering111 <bestjun111@gmail.com>
2021-07-04 18:53:37 +0900

    did some sabjil

--- game.js
+++ game.js
@@ -1,26 +1,37 @@
-var gameObject1,gameObject2,gameObject3;
 var canvas,context;
+var mouseX,mouseY;
+var grid=new Array(9);
 function startGame(){
   canvas = document.getElementById("game");
   context = canvas.getContext("2d");
 
-  gameObject1=new component(0,0,100,100,"red");
-  gameObject2 = new component(300,30,20,20,"yellow");
-  gameObject3 = new component(50,10,30,5,"blue");
 
-
+  for(let i=0;i<9;i++){
+    grid[i]=new Array(9);
+    for(let j=0;j<9;j++){
+      grid[i][j]=new component(i*60+5,j*60+5,50,50,"red","blue");
+    }
+  }
   var interval=setInterval(updateCanvas,20);
 
 }
-
-function component(x,y,width,height,color){
+function mouseMoved(e){
+  mouseX=e.pageX;
+  mouseY=e.pageY; //todo
+}
+function component(x,y,width,height,color,color_hover){
   this.x=x;
   this.y=y;
   this.width=width;
   this.height=height;
   this.color=color;
+  this.color_hover=color_hover;
   this.update=function(){
-    context.fillStyle=color;
+    if(this.x<=mouseX&&this.x+this.width>=mouseX&&this.y<=mouseY&&this.y+this.height>=mouseY){
+      context.fillStyle=color_hover;
+    }else{
+      context.fillStyle=color;
+    }
     context.fillRect(this.x,this.y,this.width,this.height);
   }
 }
@@ -28,12 +39,13 @@ function component(x,y,width,height,color){
 function updateCanvas(){
   context.clearRect(0,0,canvas.width,canvas.height);
   context.fillStyle="#99FF99";
-  context.fillRect(0,0,500,500);
+  context.fillRect(0,0,540,540);
   context.fillStyle="#0000FF";
 
-  gameObject1.update();
-  gameObject2.update();
-  gameObject3.update();
-
+  for(let i=0;i<9;i++){
+    for(let j=0;j<9;j++){
+      grid[i][j].update();
+    }
+  }
 
 }
