b71b4de912fa150de2b42b686aa0bf85ab64ec4f
powering111 <bestjun111@gmail.com>
2021-07-07 22:19:52 +0900

    created groups

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ function startGame(){
   for(let i=0;i<9;i++){
     grid[i]=new Array(9);
     for(let j=0;j<9;j++){
-      grid[i][j]=new component(i-1,j-1,i*gridsize+gridoffset,j*gridsize+gridoffset,gridsize-2*gridoffset,gridsize-2*gridoffset,"red","blue");
+      grid[i][j]=new group(i,j,(i*3*gridsize),(j*3*gridsize));
     }
   }
   var interval=setInterval(updateCanvas,20);
@@ -24,6 +24,26 @@ function mouseMoved(e){
   mouseY=Math.round(e.clientY-cRect.top); //todo
 
   document.getElementById("mousePosition").innerHTML=mouseX+", "+mouseY;
+}
+function group(x,y,left,top){
+  this.x=x;
+  this.y=y;
+  this.left=left;
+  this.top=top;
+  this.can=new Array(3);
+  for(let i=0;i<3;i++){
+    this.can[i]=new Array(3);
+    for(let j=0;j<3;j++){
+      this.can[i][j]=new component(i+1,j+1,this.left+i*gridsize+gridoffset,this.top+j*gridsize+gridoffset,gridsize-2*gridoffset,gridsize-2*gridoffset,"red","blue");
+    }
+  }
+  this.update=function(){
+    for(let i=0;i<3;i++){
+      for(let j=0;j<3;j++){
+        this.can[i][j].update();
+      }
+    }
+  }
 }
 function component(x,y,left,top,width,height,color,color_hover){
   this.x=x;
@@ -45,7 +65,7 @@ function component(x,y,left,top,width,height,color,color_hover){
   }
 }
 function clicked(e){
-  alert(now_selected.x+","now_selected.y);
+  alert(now_selected.x+","+now_selected.y);
 }
 function updateCanvas(){
   context.clearRect(0,0,canvas.width,canvas.height);
@@ -53,8 +73,8 @@ function updateCanvas(){
   context.fillRect(0,0,540,540);
   context.fillStyle="#0000FF";
 
-  for(let i=0;i<9;i++){
-    for(let j=0;j<9;j++){
+  for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
       grid[i][j].update();
     }
   }
