c35dffae20a4120b357044fecaaccff0fa5e44d5
powering111 <bestjun111@gmail.com>
2021-07-09 14:05:46 +0900

    made 'draw'

--- game.js
+++ game.js
@@ -8,14 +8,135 @@ var gameState={
   gridoffset:3,
   groupoffset:5,
   grid:new Array(9),
+  active:9,
   mouseX:0,mouseY:0,
   color_bg:"#99FF99",
   color:"white",
   color_hover:"#ffc2c2",
   color_p1:"red",
   color_p2:"blue",
-  color_disabled:"#333333"
+  color_disabled:"#333333",
+  color_draw:"#8a5a94"
 };
+function component(parent,x,y,left,top,width,height){
+  this.parent=parent;
+  this.x=x;
+  this.y=y;
+  this.left=left;
+  this.top=top;
+  this.width=width;
+  this.height=height;
+  this.state=0;
+  this.mouseHovered=function(){
+    return this.left-gameState.gridoffset<=gameState.mouseX&&this.left+this.width+gameState.gridoffset>gameState.mouseX&&this.top-gameState.gridoffset<=gameState.mouseY&&this.top+this.height+gameState.gridoffset>gameState.mouseY;
+  }
+  this.update=function(){
+    if(this.state==1){
+      gameState.context.fillStyle=gameState.color_p1;
+    }
+    else if(this.state==2){
+      gameState.context.fillStyle=gameState.color_p2;
+    }
+    else if(this.state==3){
+      gameState.context.fillStyle=gameState.color_disabled;
+    }
+    else if(this.state==4){
+      gameState.context.fillStyle=gameState.color_draw;
+    }
+    else if(this.mouseHovered()){
+      gameState.context.fillStyle=gameState.color_hover;
+      gameState.now_hovered=this;
+    }else{
+      gameState.context.fillStyle=gameState.color;
+    }
+    gameState.context.fillRect(this.left,this.top,this.width,this.height);
+  }
+  this.click=function(){
+    if(this.state==0 && this.mouseHovered()){
+      this.state=gameState.player;
+      this.parent.decreaseActive();
+      this.parent.checkMatch(this.x,this.y);
+      change_player();
+      setpossiblegrid(this.x,this.y);
+    }
+  }
+  this.setEnabled=function(){
+    if(this.state==3)
+    this.state=0;
+  }
+  this.setDisabled=function(){
+    if(this.state==0)
+      this.state=3;
+  }
+}
+function group(x,y,left,top){
+  this.x=x;
+  this.y=y;
+  this.left=left;
+  this.top=top;
+  this.active=9;
+  this.state=0;
+  this.can=new Array(3);
+  for(let i=0;i<3;i++){
+    this.can[i]=new Array(3);
+    for(let j=0;j<3;j++){
+      this.can[i][j]=new component(this,i,j,this.left+i*gameState.gridsize+gameState.gridoffset,this.top+j*gameState.gridsize+gameState.gridoffset,gameState.gridsize-2*gameState.gridoffset,gameState.gridsize-2*gameState.gridoffset);
+    }
+  }
+  this.update=function(){
+    for(let i=0;i<3;i++){
+      for(let j=0;j<3;j++){
+        this.can[i][j].update();
+      }
+    }
+  }
+  this.setEnabled=function(){
+    for(let i=0;i<3;i++){
+      for(let j=0;j<3;j++){
+        this.can[i][j].setEnabled();
+      }
+    }
+  }
+  this.setDisabled=function(){
+    for(let i=0;i<3;i++){
+      for(let j=0;j<3;j++){
+        this.can[i][j].setDisabled();
+      }
+    }
+  }
+  this.checkMatch=function(x,y){
+    let winner = isMatch(this.can,x,y);
+    if(winner!=0){
+      this.win(winner,x,y);
+    }
+  }
+  this.decreaseActive=function(){
+    this.active--;
+    if(this.active==0){
+      for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+          this.can[i][j].state=4;
+        }
+      }
+      gameState.active--;
+      if(gameState.active==0){
+        UltimateDraw();
+      }
+    }
+  }
+
+  this.win=function(player){
+      this.active=0;
+      this.state =player;
+      for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+          this.can[i][j].state=player;
+        }
+      }
+      checkUltimateMatch(this.x,this.y);
+  }
+}
+
 function startGame(){
   gameState.canvas = document.getElementById("game");
   gameState.context = gameState.canvas.getContext("2d");
@@ -30,13 +151,19 @@ function startGame(){
   var interval=setInterval(updateCanvas,20);
 
 }
-function mouseMoved(e){
-  let cRect= gameState.canvas.getBoundingClientRect();
-  gameState.mouseX=Math.round(e.clientX-cRect.left);
-  gameState.mouseY=Math.round(e.clientY-cRect.top);
+function updateCanvas(){
+  gameState.context.clearRect(0,0,gameState.canvas.width,gameState.canvas.height);
+  gameState.context.fillStyle=gameState.color_bg;
+  gameState.context.fillRect(0,0,550,550);
 
-  //document.getElementById("mousePosition").innerHTML=gameState.mouseX+", "+gameState.mouseY;
+  for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
+      gameState.grid[i][j].update();
+    }
+  }
+
 }
+
 function isMatch(arr,x,y){
   let cnt=0;
   for(let i=0;i<3;i++){
@@ -98,63 +225,12 @@ function checkUltimateMatch(x,y){
   }
 }
 function UltimateWin(winner){
-
   setTimeout(function(){alert("Player "+winner+" 승리!");},500);
 }
-function group(x,y,left,top){
-  this.x=x;
-  this.y=y;
-  this.left=left;
-  this.top=top;
-  this.active=9;
-  this.state=0;
-  this.can=new Array(3);
-  for(let i=0;i<3;i++){
-    this.can[i]=new Array(3);
-    for(let j=0;j<3;j++){
-      this.can[i][j]=new component(this,i,j,this.left+i*gameState.gridsize+gameState.gridoffset,this.top+j*gameState.gridsize+gameState.gridoffset,gameState.gridsize-2*gameState.gridoffset,gameState.gridsize-2*gameState.gridoffset);
-    }
-  }
-  this.update=function(){
-    for(let i=0;i<3;i++){
-      for(let j=0;j<3;j++){
-        this.can[i][j].update();
-      }
-    }
-  }
-  this.setEnabled=function(){
-    for(let i=0;i<3;i++){
-      for(let j=0;j<3;j++){
-        this.can[i][j].setEnabled();
-      }
-    }
-  }
-  this.setDisabled=function(){
-    for(let i=0;i<3;i++){
-      for(let j=0;j<3;j++){
-        this.can[i][j].setDisabled();
-      }
-    }
-  }
-  this.checkMatch=function(x,y){
-    let winner = isMatch(this.can,x,y);
-    if(winner!=0){
-      this.win(winner,x,y);
-    }
-
-  }
-
-  this.win=function(player){
-      this.active=0;
-      this.state =player;
-      for(let i=0;i<3;i++){
-        for(let j=0;j<3;j++){
-          this.can[i][j].state=player;
-        }
-      }
-      checkUltimateMatch(this.x,this.y);
-  }
+function UltimateDraw(){
+  setTimeout(function(){alert("무승부");},500);
 }
+
 function setpossiblegrid(x,y){
   if(gameState.grid[x][y].active!=0){
     gameState.possiblegrid={x:x,y:y};
@@ -172,54 +248,6 @@ function setpossiblegrid(x,y){
     }
   }
 }
-function component(parent,x,y,left,top,width,height){
-  this.parent=parent;
-  this.x=x;
-  this.y=y;
-  this.left=left;
-  this.top=top;
-  this.width=width;
-  this.height=height;
-  this.state=0;
-  this.mouseHovered=function(){
-    return this.left-gameState.gridoffset<=gameState.mouseX&&this.left+this.width+gameState.gridoffset>gameState.mouseX&&this.top-gameState.gridoffset<=gameState.mouseY&&this.top+this.height+gameState.gridoffset>gameState.mouseY;
-  }
-  this.update=function(){
-    if(this.state==1){
-      gameState.context.fillStyle=gameState.color_p1;
-    }
-    else if(this.state==2){
-      gameState.context.fillStyle=gameState.color_p2;
-    }
-    else if(this.state==3){
-      gameState.context.fillStyle=gameState.color_disabled;
-    }
-    else if(this.mouseHovered()){
-      gameState.context.fillStyle=gameState.color_hover;
-      gameState.now_hovered=this;
-    }else{
-      gameState.context.fillStyle=gameState.color;
-    }
-    gameState.context.fillRect(this.left,this.top,this.width,this.height);
-  }
-  this.click=function(){
-    if(this.state==0 && this.mouseHovered()){
-      this.state=gameState.player;
-      this.parent.active--;
-      this.parent.checkMatch(this.x,this.y);
-      change_player();
-      setpossiblegrid(this.x,this.y);
-    }
-  }
-  this.setEnabled=function(){
-    if(this.state==3)
-    this.state=0;
-  }
-  this.setDisabled=function(){
-    if(this.state==0)
-      this.state=3;
-  }
-}
 function change_player(){
   if(gameState.player==1){
     gameState.player=2;
@@ -231,18 +259,12 @@ function change_player(){
   document.getElementById("player").innerHTML="Player "+gameState.player;
 
 }
+
+function mouseMoved(e){
+  let cRect= gameState.canvas.getBoundingClientRect();
+  gameState.mouseX=Math.round(e.clientX-cRect.left);
+  gameState.mouseY=Math.round(e.clientY-cRect.top);
+}
 function clicked(e){
   gameState.now_hovered.click();
-}
-function updateCanvas(){
-  gameState.context.clearRect(0,0,gameState.canvas.width,gameState.canvas.height);
-  gameState.context.fillStyle=gameState.color_bg;
-  gameState.context.fillRect(0,0,550,550);
-
-  for(let i=0;i<3;i++){
-    for(let j=0;j<3;j++){
-      gameState.grid[i][j].update();
-    }
-  }
-
 }
